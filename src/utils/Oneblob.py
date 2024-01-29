8e210b9926b815c712197f1944745eb6b8360bb4
HaoyangSunMartin <sunhaoyangmartin@gmail.com>
2024-01-29 11:25:10 +0100

    One Blob Implementation
    This is the one blob encoding. It is a subclass of torch.nn.Modules, just import and use as a torch.nn module. You need to define the upper_bound and the lower_bound
    This module takes in a batch of 3D coordinates(of size (B,3) ) within the bound, and then returns an encoded tensor of size (B, 3*num_bins).

---
+++ src/utils/Oneblob.py
@@ -0,0 +1,40 @@
+import torch
+
+class One_Blob(torch.nn.Module):
+   def __init__(self, lower_bound= -100.0, upper_bound=-100.0, num_bins=1024,std_dev = 1.0):
+        #one blob encoding encodes (B,3) tensor into (B,3*1024) tensor
+        #one blob calculates the distance between the current bin and the kernel value
+        #then assigns the kernel value to the bin according to a gaussian distribution
+        super().__init__()
+        self.gaussian = torch.distributions.normal.Normal(0,std_dev)
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self.num_bins = num_bins
+        self.bin_size = (self.upper_bound - self.lower_bound) / self.num_bins
+        self.bin_loc = torch.arange(self.lower_bound, self.upper_bound, self.bin_size)+self.bin_size/2
+        self.bins = torch.arange(self.lower_bound, self.upper_bound, self.bin_size)
+        self.gaussian.requires_grad = False
+
+        # this function calculates the distance between the current bin and the kernel value
+
+   def forward(self, x):
+        # x is a (B,3) tensor
+        if len(x.shape) == 1:
+            x = x.unsqueeze(0)
+        batch_size = x.shape[0]
+        dis = self.bin_loc.repeat(3, 1)
+        dis = dis.repeat(batch_size, 1, 1)
+        # dis is a (B,3,1024) tensor
+        x = x.unsqueeze(-1)
+        x = x.repeat(1, 1, self.num_bins)
+        dis = dis - x
+        blob = self.gaussian.log_prob(dis)
+        blob = torch.exp(blob)
+        print("hi")
+        blob = blob.reshape(batch_size, -1)
+
+        return blob
+
+
+
+
